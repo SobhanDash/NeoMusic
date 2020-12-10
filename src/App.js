e27c48f0824be0ccb716de6dbb8e2132dce3e08e
Sobhan Dash <52308005+SobhanDash@users.noreply.github.com>
2020-12-10 13:59:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./styles.css";
+import { musicDB } from "./MusicDB";
+
+// for accessing the keys
+var musicdict = Object.keys(musicDB);
+
+export default function App() {
+  // initial state load is at English
+  const [selectedGenre, setSelectedGenre] = useState("English");
+
+  // Click Handler => selects the genre
+  function genreClickHandler(genre) {
+    setSelectedGenre(genre);
+  }
+
+  return (
+    <div className="App">
+      <h1>🎧Feel the Music!</h1>
+      <div>
+        {/* renders the buttons at first render */}
+        {musicdict.map((genre) => (
+          // sets genre by button click
+          <button
+            onClick={() => genreClickHandler(genre)}
+            className="genreButton"
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+
+      {/* return the list */}
+      <div style={{ textAlign: "left" }}>
+        <ul style={{ paddingInlineStart: "0" }}>
+          {musicdict[selectedGenre].map((music) => (
+            <li key={music.name} className="list">
+              {music.name}
+              {music.rating}
+              {music.singer}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ src/MusicDB.js
@@ -0,0 +1,43 @@
+export let musicDB = {
+  English: [
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" }
+  ],
+
+  Hindi: [
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" }
+  ],
+
+  RnB: [
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" }
+  ],
+
+  Pop: [
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" }
+  ],
+
+  Rock: [
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" }
+  ],
+
+  Rap: [
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" },
+    { name: "", rating: "", singer: "" }
+  ]
+};
